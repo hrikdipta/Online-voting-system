51dda6601e48334805af6255e03f923f6b7cf38b
Hrikdipta Kundu <hrikdiptakundu123@gmail.com>
2024-05-09 16:42:16 +0530

    insatlled flowbite react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import { Button } from 'flowbite-react'
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className='text-red-500'>Hello</div>
+  return(
+    <Button gradientDuoTone="purpleToBlue">Purple to Blue</Button>
   )
+ 
 }
 
 export default App
