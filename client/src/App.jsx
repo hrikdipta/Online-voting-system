1608083ae1481d8b44445a25a047c74608112768
Hrikdipta Kundu <hrikdiptakundu123@gmail.com>
2024-05-11 01:55:09 +0530

    created navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
-
-import { Button } from 'flowbite-react'
+import Navbar_component from './components/Navbar'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return(
-    <Button gradientDuoTone="purpleToBlue">Purple to Blue</Button>
+    <BrowserRouter>
+      <Navbar_component/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About />} />
+        <Route path='/instructions' element={<Instructions />} />
+        <Route path='/news' element={<News />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/voter/login' element={<VoterLogin />} />
+        <Route path='/admin/login' element={<AdminLogin />} />
+      </Routes>
+    </BrowserRouter>
   )
  
 }
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { useLocation,Link } from 'react-router-dom';
+import { Navbar,Button } from "flowbite-react";
+function Navbar_component() {
+  const path = useLocation().pathname;
+
+  return (
+    <Navbar fluid rounded>
+      <Navbar.Brand href="#"as={'div'}>
+        <Link to={'/'} className='flex'>
+        <img src="https://www.shutterstock.com/shutterstock/photos/2184667969/display_1500/stock-vector-ballot-box-logo-simple-icon-vector-flat-design-ai-2184667969.jpg" className="mr-3 h-6 sm:h-9" alt="Logo" />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">ElectionHub</span>
+        </Link>
+      </Navbar.Brand>
+      <div className="flex md:order-2">
+        <div className='hidden md:inline-flex'>
+          <Link to='/voter/login'><Button outline gradientDuoTone="pinkToOrange" className='mr-2'>Voter Login</Button></Link>
+          <Link to='/admin/login'><Button outline gradientDuoTone="redToYellow">Admin Login</Button></Link>
+        </div>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link href="#" active={path==='/'?true:false} as={'div'}>
+          <Link to='/' >Home</Link>
+        </Navbar.Link>
+        <Navbar.Link href="#" active={path==='/about'?true:false} as={'div'}>
+          <Link to='/about' >About</Link>
+        </Navbar.Link>
+        <Navbar.Link href="#" active={path==='/instructions'?true:false} as={'div'}>
+          <Link to='/instructions' >Instructions</Link>
+        </Navbar.Link>
+        <Navbar.Link href="#" active={path==='/news'?true:false} as={'div'}>
+          <Link to='/news' >News</Link>
+        </Navbar.Link>
+        <Navbar.Link href="#" active={path==='/contact'?true:false} as={'div'}>
+          <Link to='/contact' >Contact</Link>
+        </Navbar.Link>
+        <div className='inline-flex md:hidden'>
+          <Link to='/voter/login'><Button outline gradientDuoTone="pinkToOrange" className='mr-2'>Voter Login</Button></Link>
+          <Link to='/admin/login'><Button outline gradientDuoTone="redToYellow">Admin Login</Button></Link>
+        </div>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+export default Navbar_component
